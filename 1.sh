df70e8c48008d1a6c9c87f6d4af18acd54cdba23
MQN-80 <3218290253@qq.com>
2022-07-31 22:27:00 +0800

    da

--- 1.sh
+++ 1.sh
@@ -1,42 +1,33 @@
 #!/bin/sh
 git filter-branch -f --env-filter '
-
 an="$梦秋年"
 am="$oncwnuFi0ujwfi5-C_5zg7Z0IblM@git.weixin.qq.com"
 cn="$MQN-80"
 cm="$3218290253@qq.com"
-
-
 oldName="old_sssss"
 oldEmail="old_sssss@gmail.com"
 newName="Peter"
 newEmail="Peter@gmail.com"
-
 if [ "$GIT_COMMITTER_EMAIL" = "$oldEmail" ]
 then
     cn="$newName"
     cm="$newEmail"
 fi
-
 if [ "$GIT_COMMITTER_NAME" -eq "$oldName" ]
 then
     cn="$newName"
     cm="$newEmail"
 fi
-
-
 if [ "$GIT_AUTHOR_EMAIL" = "$oldEmail" ]
 then
     an="$newName"
     am="$newEmail"
 fi
-
 if [ "$GIT_AUTHOR_NAME" -eq "$oldName" ]
 then
     an="$newName"
     am="$newEmail"
 fi
-
 export GIT_AUTHOR_NAME="$an"
 export GIT_AUTHOR_EMAIL="$am"
 export GIT_COMMITTER_NAME="$cn"
