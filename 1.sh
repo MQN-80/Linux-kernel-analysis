437a07bb0ea0133cf425fdddbfc6979437ac4105
MQN-80 <3218290253@qq.com>
2022-07-31 22:32:45 +0800

    debug

--- 1.sh
+++ 1.sh
@@ -1,35 +1,19 @@
 #!/bin/sh
-git filter-branch -f --env-filter '
-an="$梦秋年"
-am="$oncwnuFi0ujwfi5-C_5zg7Z0IblM@git.weixin.qq.com"
-cn="$MQN-80"
-cm="$3218290253@qq.com"
-oldName="old_sssss"
-oldEmail="old_sssss@gmail.com"
-newName="Peter"
-newEmail="Peter@gmail.com"
-if [ "$GIT_COMMITTER_EMAIL" = "$oldEmail" ]
+
+git filter-branch --env-filter '
+
+OLD_EMAIL="oncwnuFi0ujwfi5-C_5zg7Z0IblM@git.weixin.qq.com"
+CORRECT_NAME="MQN-80"
+CORRECT_EMAIL="3218290253@qq.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
 then
-    cn="$newName"
-    cm="$newEmail"
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
 fi
-if [ "$GIT_COMMITTER_NAME" -eq "$oldName" ]
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
 then
-    cn="$newName"
-    cm="$newEmail"
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
 fi
-if [ "$GIT_AUTHOR_EMAIL" = "$oldEmail" ]
-then
-    an="$newName"
-    am="$newEmail"
-fi
-if [ "$GIT_AUTHOR_NAME" -eq "$oldName" ]
-then
-    an="$newName"
-    am="$newEmail"
-fi
-export GIT_AUTHOR_NAME="$an"
-export GIT_AUTHOR_EMAIL="$am"
-export GIT_COMMITTER_NAME="$cn"
-export GIT_COMMITTER_EMAIL="$cm"
-'
+' --tag-name-filter cat -- --branches --tags
