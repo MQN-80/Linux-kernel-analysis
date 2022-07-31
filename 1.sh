9f34bc20c6e82a71a1a42a091cfb93aeb2f09f02
MQN-80 <3218290253@qq.com>
2022-07-31 22:19:56 +0800

    ddwq

---
+++ 1.sh
@@ -0,0 +1,18 @@
+  #!/bin/sh
+  git filter-branch --env-filter '
+ 
+  OLD_EMAIL="oncwnuFi0ujwfi5-C_5zg7Z0IblM@git.weixin.qq.com"
+  CORRECT_NAME="MQN-80"
+  CORRECT_EMAIL="3218290253@qq.com"
+ 
+  if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+  then
+  export GIT_COMMITTER_NAME="$CORRECT_NAME"
+  export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+  fi
+  if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+  then
+  export GIT_AUTHOR_NAME="$CORRECT_NAME"
+  export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+  fi
+  ' --tag-name-filter cat -- --branches --tags
